bba507db2c8be241dafae5a796b78e612ebb489e
markadillman <dillmanm@oregonstate.edu>
2017-05-07 10:10:58 -0400

    edit POST save

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'hbs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,6 +29,12 @@ app.use('/users', users);
 
 //make process trackable
 process.title = "ariesApp";
+
+//handle submitted tile edit requests
+app.post('/edit',function(req,res){
+	var rawSVG = req.body.svg;
+	console.log(rawSVG);
+})
 
 
 // catch 404 and forward to error handler
