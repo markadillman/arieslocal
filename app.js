4be91503b23e29ed8e574fb2da0f7e9cc627deaf
markadillman <dillmanm@oregonstate.edu>
2017-05-07 18:26:41 -0400

    db begin

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.post('/edit',function(req,res){
 	rawSVG = rawSVG.slice(0,(rawSVG.length-6));
 	console.log(rawSVG);
 	//construct query to send to db
-	var insertDoc = {};
+	var insertDoc = {xcood,ycoord,pw,svg};
 	insertDoc.xcoord = xcoord;
 	insertDoc.ycoord = ycoord;
 	insertDoc.pw = pw;
