5f82871aea68573a3ac9e6d5478e999d6fbe1c2b
markadillman <dillmanm@oregonstate.edu>
2017-05-07 17:34:16 -0400

    db begin

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.post('/edit',function(req,res){
 	insertDoc["ycoord"] = ycoord;
 	insertDoc["pw"] = pw;
 	insertDoc["svg"] = svg;
-	console.log(insertDoc);
+	console.log(JSON.stringify(insertDoc));
 	MongoClient.connect(dbUrl,function(err,db){
 		//test for errors, pop out if there are errors present
 		assert.equal(null,err);
