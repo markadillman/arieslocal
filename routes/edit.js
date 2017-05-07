d21833af2cdfd57ae5a9937601dd722d79c63235
markadillman <dillmanm@oregonstate.edu>
2017-05-07 10:35:16 -0400

    edit POST save

---
+++ routes/edit.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//POST to handle edits
+router.post('/edit',function(req,res){
+	var rawSVG = req.body.svg;
+	console.log(rawSVG);
+	//package into a mongoDB query
+});
+
+module.exports = function(req,res){
+	var rawSVG = req.body.svg;
+	console.log(rawSVG);
+	//package into a mongoDB query
+});
